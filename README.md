df7b2a72f7a7b5d308ee9ab64df68f58c47a1377
joshuaai <joshuaiheji@gmaill.com>
2018-08-23 09:25:57 +0100

    Initial drag and drop with react-beautiful-dnd and optimistic UI updates

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ A Trello-inspired view for managing todos built with [React Beautiful DnD](https
 
 ## Tools and Packages
 
-| Dependency | Purpose |
-|------------|---------|
-| [@atlaskit/css-reset](https://www.npmjs.com/package/@atlaskit/css-reset) | Minified set of CSS rules for visual consistency |
-| [styled-components](https://github.com/styled-components/styled-components) | Component-scoped CSS-in-JS framework using tagged template literals |
+| S/N | Dependency | Purpose |
+|-----|------------|---------|
+| 1. | [@atlaskit/css-reset](https://www.npmjs.com/package/@atlaskit/css-reset) | Minified set of CSS rules for visual consistency. |
+| 2. | [styled-components](https://github.com/styled-components/styled-components) | Component-scoped CSS-in-JS framework using tagged template literals. |
+| 3. | [react-beautiful-dnd](https://github.com/atlassian/react-beautiful-dnd) | Beautiful and accessible drag and drop for lists with React |
--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '@atlaskit/css-reset'
+import { DragDropContext } from 'react-beautiful-dnd'
 import initialData from './initial-data'
 import Column from './column'
 
 class App extends React.Component {
   state = initialData
 
+  onDragEnd = result => {
+    const { destination, source, draggableId } = result
+
+    if (!destination) return
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    const column = this.state.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    // move the task id from its old index to its new index
+    newTaskIds.splice(source.index, 1) // remove the id
+    newTaskIds.splice(destination.index, 0, draggableId) // re-add it to new position
+
+    const newColumn = { ...column, taskIds: newTaskIds }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    }
+
+    this.setState(newState)
+  }
+
   render() {
-    return this.state.columnOrder.map(columdId => {
-      const column = this.state.columns[columdId]
-      const tasks = column.taskIds.map(tasksId => this.state.tasks[tasksId])
-      return <Column key={column.id} column={column} tasks={tasks} />
-    })
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columdId => {
+          const column = this.state.columns[columdId]
+          const tasks = column.taskIds.map(tasksId => this.state.tasks[tasksId])
+          return <Column key={column.id} column={column} tasks={tasks} />
+        })}
+      </DragDropContext>
+    )
   }
 }
 
